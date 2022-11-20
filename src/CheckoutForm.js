961f7bcbdcab84dc5a994d9bdb67f2b416b01cce
Yann Mainier <yann.mainier@gmail.com>
2022-11-20 22:44:34 +0000

    landmark

--- src/CheckoutForm.js
+++ src/CheckoutForm.js
@@ -5,6 +5,7 @@ import { PaymentMethod } from "./PaymentMethod";
 export function CheckoutForm() {
   return (
     <form onSubmit={(e) => e.preventDefault()}>
+      <h1>Checkout</h1>
       <PersonalInfo />
       <Address />
       <PaymentMethod />
