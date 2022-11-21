ea0fd33cd2f1e063dcc98023bdcdf2ee0fb0f18a
Yann Mainier <yann.mainier@gmail.com>
2022-11-21 21:30:27 +0000

    add pattern to input

--- src/PaymentMethod.js
+++ src/PaymentMethod.js
@@ -1,4 +1,4 @@
-import { useId } from "react"
+import { useId, useState } from "react";
 
 function Terms() {
   const id = useId();
@@ -11,48 +11,79 @@ function Terms() {
 }
 
 export function PaymentMethod() {
+  const [currentTab, setCurrentTab] = useState("creditcard");
   return (
     <div>
-      <fieldset>
-        <legend>Card Payment</legend>
-        <div className="form-line">
-          <label htmlFor="card_number">Card Number:</label>
-          <input
-            type="text"
-            autofill="cc-number"
-            name="card_number"
-            id="card_number"
-            pattern="[0-9]{15,16}"
-            required
-          />
+      <div className="tab-list">
+        <button
+          className="tab"
+          type="button"
+          onClick={() => setCurrentTab("creditcard")}
+          data-selected={currentTab === "creditcard"}
+        >
+          Credit card
+        </button>
+        <button
+          className="tab"
+          type="button"
+          onClick={() => setCurrentTab("paypal")}
+          data-selected={currentTab === "paypal"}
+        >
+          Paypal
+        </button>
+      </div>
+      <div className="tab-panel-list">
+        <div className="panel" hidden={currentTab !== "creditcard"}>
+          <fieldset>
+            <legend>Card Payment</legend>
+            <div className="form-line">
+              <label htmlFor="card_number">Card Number:</label>
+              <input
+                type="text"
+                autofill="cc-number"
+                name="card_number"
+                id="card_number"
+                pattern="[0-9]{15,16}"
+                required
+              />
+            </div>
+            <div className="form-line">
+              <label htmlFor="expiry_date">Expiry date:</label>
+              <input
+                type="month"
+                autofill="cc-exp"
+                name="expiry_date"
+                id="expiry_date"
+                required
+              />
+            </div>
+            <div className="form-line">
+              <label htmlFor="security_code">Security code:</label>
+              <input
+                type="text"
+                autofill="cc-csc"
+                name="security_code"
+                id="security_code"
+                pattern="[0-9]{3,4}"
+                required
+              />
+            </div>
+            <Terms />
+            <div className="form-line">
+              <button>Checkout</button>
+            </div>
+          </fieldset>
         </div>
-        <div className="form-line">
-          <label htmlFor="expiry_date">Expiry date:</label>
-          <input type="month" autofill="cc-exp" name="expiry_date" id="expiry_date" required />
+        <div className="panel" hidden={currentTab !== "paypal"}>
+          <fieldset>
+            <legend>Paypal</legend>
+            <Terms />
+            <div className="form-line">
+              <button>Checkout with Paypal</button>
+            </div>
+          </fieldset>
         </div>
-        <div className="form-line">
-          <label htmlFor="security_code">Security code:</label>
-          <input
-            type="text"
-            autofill="cc-csc"
-            name="security_code"
-            id="security_code"
-            pattern="[0-9]{3,4}"
-            required
-          />
-        </div>
-        <Terms />
-        <div className="form-line">
-          <button>Checkout</button>
-        </div>
-      </fieldset>
-      <fieldset>
-        <legend>Paypal</legend>
-        <Terms />
-        <div className="form-line">
-          <button>Checkout with Paypal</button>
-        </div>
-      </fieldset>
+      </div>
     </div>
   );
 }
