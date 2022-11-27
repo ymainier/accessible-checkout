eebadd1c8a3ae5efe4a5354a2ab3fd40d9345f91
Yann Mainier <yann.mainier@gmail.com>
2022-11-27 17:04:27 +0000

    tweaking autocomplete

--- src/Address.tsx
+++ src/Address.tsx
@@ -4,17 +4,17 @@ export function Address() {
       <legend>Address</legend>
       <div className="form-line">
         <label htmlFor="house_number">House number:</label>
-        <input type="text" name="house_number" id="house_number" required />
+        <input type="text" autoComplete="address-line1" name="house_number" id="house_number" required />
       </div>
       <div className="form-line">
         <label htmlFor="street">Street:</label>
-        <input type="text" name="street" id="street" />
+        <input type="text" autoComplete="address-line3" name="street" id="street" />
       </div>
       <div className="form-line">
         <label htmlFor="city">City:</label>
         <input
           type="text"
-          autoComplete="address-level1"
+          autoComplete="address-level2"
           name="city"
           id="city"
           required
