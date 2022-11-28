fb14af24929e87c5902264e2afe3a1ec6cd14b6a
Yann Mainier <yann.mainier@gmail.com>
2022-11-28 21:41:30 +0000

    add a disclosure to reveal the voucher form

--- src/VoucherForm.tsx
+++ src/VoucherForm.tsx
@@ -1,16 +1,30 @@
+import * as Collapsible from "@radix-ui/react-collapsible";
+
 export function VoucherForm() {
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
-      <h3>Voucher</h3>
-      <div className="voucher">
-        <div className="form-line">
-          <label htmlFor="voucher">Enter voucher code: </label>
-          <input type="text" name="voucher" id="voucher" required autoComplete="off" />
-        </div>
-        <div className="form-line">
-          <button type="button">Apply</button>
-        </div>
-      </div>
-    </form>
+    <Collapsible.Root className="voucher-disclosure">
+      <Collapsible.Trigger>
+        <h3>Voucher</h3>
+      </Collapsible.Trigger>
+      <Collapsible.Content>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <div className="voucher">
+            <div className="form-line">
+              <label htmlFor="voucher">Enter voucher code: </label>
+              <input
+                type="text"
+                name="voucher"
+                id="voucher"
+                required
+                autoComplete="off"
+              />
+            </div>
+            <div className="form-line">
+              <button type="button">Apply</button>
+            </div>
+          </div>
+        </form>
+      </Collapsible.Content>
+    </Collapsible.Root>
   );
 }
